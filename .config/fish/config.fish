fe79548dacffd06c3b2b7403c84270e0bf279d0f
Enrique Pando <enr.pando@gmail.com>
2022-09-14 21:12:28 +0000

    install neofetch and fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,31 @@
+# set fish_greeting
+set -g theme_display_git_default_branch yes
+set -g theme_display_user yes
+set -g theme_display_hostname yes
+set -g theme_date_format "+%H:%M:%S"
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '> '
+
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function ranger-cd
+  set tempfile '/tmp/chosendir'
+  ranger --choosedir=$tempfile (pwd)
+
+  if test -f $tempfile
+      if [ (cat $tempfile) != (pwd) ]
+        cd (cat $tempfile)
+      end
+  end
+
+  rm -f $tempfile
+end
+
+function fish_user_key_bindings
+    bind \co 'ranger-cd ; commandline -f repaint'
+end
---
+++ .config/fish/functions/fish_greeting.fish
@@ -0,0 +1,6 @@
+function fish_greeting
+  neofetch
+  # set_color $fish_color_autosuggestion
+  # echo "I'm completely operational, and all my circuits are functioning perfectly."
+  # set_color normal
+end

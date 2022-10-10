7414da077ced2b31bf5837bfa6c0553ad2bf2bbe
Enrique Pando <enr.pando@gmail.com>
2022-10-09 21:04:42 -0500

    awesome: show seconds in textclock

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -254,7 +254,7 @@ awful.screen.connect_for_each_screen(function(s)
         },
         {
             layout = wibox.layout.fixed.horizontal,
-            wibox.widget.textclock(),
+            wibox.widget.textclock("%a %b %d, %H:%M:%S", 1),
         },
         -- s.mytasklist,
         {

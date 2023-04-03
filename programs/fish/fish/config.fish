ea992fa1d35d50631a293d13230d58f7cfcae73b
Enrique Pando <enr.pando@gmail.com>
2023-04-03 11:45:00 -0600

    Add fish git abbreviations

--- programs/fish/fish/config.fish
+++ programs/fish/fish/config.fish
@@ -1,6 +1,10 @@
 set -g fish_prompt_pwd_dir_length 0
 
-alias logout="kill -9 -1"
+abbr logout kill -9 -1
+abbr gs git status
+abbr gl git log
+abbr gd git diff
+abbr ga git add
 
 if status is-interactive
   # Commands to run in interactive sessions can go here

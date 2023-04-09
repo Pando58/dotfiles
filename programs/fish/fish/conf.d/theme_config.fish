8bf4ca8081c43df494824934ceed09a0a2064fe0
Enrique Pando <enr.pando@gmail.com>
2023-04-08 21:45:29 -0600

    Set fish shell theme

---
+++ programs/fish/fish/conf.d/theme_config.fish
@@ -0,0 +1,11 @@
+set -g theme_color_scheme terminal
+
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt "\$ "
+
+set -g theme_title_display_process yes
+
+set -g theme_date_format "+%H:%M:%S"
+set -g theme_right_prompt_glyph "·"
+
+set -g theme_cmd_duration_min 0 # in ms

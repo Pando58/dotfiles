700175d9f4e77fc07641dc616aad45bf39ad29a5
Enrique Pando <enr.pando@gmail.com>
2023-04-16 17:43:38 -0600

    Add pkgs-unstable

--- flake.nix
+++ flake.nix
@@ -3,26 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {
     nixpkgs,
+    nixpkgs-unstable,
     home-manager,
     ...
   }: let
     system = "x86_64-linux";
     stateVersion = "22.11";
     hostname = "nixos";
+    pkgs-unstable = import nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
+    };
   in {
     nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
       inherit system;
-
-      specialArgs = { inherit hostname stateVersion; };
+      specialArgs = { inherit pkgs-unstable hostname stateVersion; };
 
       modules = [
         home-manager.nixosModules.home-manager
+        { home-manager.extraSpecialArgs = { inherit pkgs-unstable; }; }
         machines/main
       ];
     };

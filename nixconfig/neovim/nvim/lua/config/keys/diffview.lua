2ab0345865e88ef7eaa0048632f03d7df057ca90
Enrique pando <enr.pando@gmail.com>
2024-12-14 13:35:52 -0600

    Add neovim diffview git history keymap

--- nixconfig/neovim/nvim/lua/config/keys/diffview.lua
+++ nixconfig/neovim/nvim/lua/config/keys/diffview.lua
@@ -1,6 +1,7 @@
 return {
 	keys = function ()
-		vim.keymap.set("n", "<leader>vo", ":DiffviewOpen<CR>", { desc = "Show diff view" })
-		vim.keymap.set("n", "<leader>vc", ":DiffviewClose<CR>", { desc = "Close diff view" })
+		vim.keymap.set("n", "<leader>vo", ":DiffviewOpen<CR>", { desc = "Open diffview" })
+		vim.keymap.set("n", "<leader>vc", ":DiffviewClose<CR>", { desc = "Close diffview" })
+		vim.keymap.set("n", "<leader>vh", ":DiffviewFileHistory<CR>", { desc = "Show diffview git history" })
 	end,
 }

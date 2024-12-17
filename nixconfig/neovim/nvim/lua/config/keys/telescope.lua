43b2811e5fc5cbe721a6c64a5a6731ca3fa716e0
Enrique pando <enr.pando@gmail.com>
2024-12-17 12:29:15 -0600

    change telescope keymaps

--- nixconfig/neovim/nvim/lua/config/keys/telescope.lua
+++ nixconfig/neovim/nvim/lua/config/keys/telescope.lua
@@ -9,8 +9,8 @@ end
 return {
 	keys = function ()
 		map("<leader>ss", telescope_builtin.builtin, { desc = "[s]earch built-in pickers" })
-		map("<leader>sb", telescope_builtin.current_buffer_fuzzy_find, { desc = "[s]earch in current [b]uffer" })
-		map("<leader>sB", telescope_builtin.buffers, { desc = "[s]earch [B]uffers" })
+		map("<leader>sB", telescope_builtin.current_buffer_fuzzy_find, { desc = "[s]earch in current [b]uffer" })
+		map("<leader>sb", telescope_builtin.buffers, { desc = "[s]earch [B]uffers" })
 		map("<leader>sf", function () telescope_builtin.find_files({ hidden = true }) end, { desc = "[s]earch [f]iles in current directory" })
 		map("<leader>sF", telescope_builtin.oldfiles, { desc = "[s]earch old [F]iles" })
 		map("<leader>sh", telescope_builtin.help_tags, { desc = "[s]earch [h]elp" })
@@ -22,10 +22,10 @@ return {
 		map("<leader>slsb", telescope_builtin.lsp_document_symbols, { desc = "[s]earch [l]sp [s]ymbols in the current [b]uffer" })
 		map("<leader>slsw", telescope_builtin.lsp_workspace_symbols, { desc = "[s]earch [l]sp [s]ymbols in the current [w]orkspace" })
 		map("<leader>st", telescope_builtin.treesitter, { desc = "[s]earch stuff from treesitter" })
-		map("<leader>sGs", telescope_builtin.git_status, { desc = "[s]earch [G]it [s]tatus" })
-		map("<leader>sGS", telescope_builtin.git_stash, { desc = "[s]earch [G]it [s]tash" })
-		map("<leader>sGc", telescope_builtin.git_commits, { desc = "[s]earch [G]it [c]ommits" })
-		map("<leader>sGb", telescope_builtin.git_branches, { desc = "[s]earch [G]it [b]ranches" })
+		map("<leader>gss", telescope_builtin.git_status, { desc = "[g]it [s]earch [s]tatus" })
+		map("<leader>gst", telescope_builtin.git_stash, { desc = "[g]it [s]earch s[t]ash" })
+		map("<leader>gsc", telescope_builtin.git_commits, { desc = "[g]it [s]earch [c]ommits" })
+		map("<leader>gsb", telescope_builtin.git_branches, { desc = "[g]it [s]earch [b]ranches" })
 
 		map("<leader>su", telescope.extensions.undo.undo, { desc = "[s]earch [u]ndo tree" })
 

ba9024226ef323e23b1f482a6769f7966dea9a34
Enrique Pando <enr.pando@gmail.com>
2025-10-31 13:32:22 -0600

    disable nvim diagnostic icons

--- config/common/home/.config/nvim/lua/config/options.lua
+++ config/common/home/.config/nvim/lua/config/options.lua
@@ -59,7 +59,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Diagnostic icons
-vim.fn.sign_define("DiagnosticSignError", {text = " ", texthl = "DiagnosticSignError"})
+--[[ vim.fn.sign_define("DiagnosticSignError", {text = " ", texthl = "DiagnosticSignError"})
 vim.fn.sign_define("DiagnosticSignWarn", {text = " ", texthl = "DiagnosticSignWarn"})
 vim.fn.sign_define("DiagnosticSignInfo", {text = " ", texthl = "DiagnosticSignInfo"})
-vim.fn.sign_define("DiagnosticSignHint", {text = "󰌵", texthl = "DiagnosticSignHint"})
+vim.fn.sign_define("DiagnosticSignHint", {text = "󰌵", texthl = "DiagnosticSignHint"}) ]]
